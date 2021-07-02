ae7f3158f77f5118a93d450e1d5f19ec7f3b46d0
Prashant Shukla <123shuklaprashant@gmail.com>
2021-07-02 10:03:00 +0530

    data according to url

---
+++ TikTok/app/src/main/java/com/app/tiktok/model/Games.kt
@@ -0,0 +1,7 @@
+package com.app.tiktok.model
+
+class Games {
+
+    var success: `val`? = null: Boolean,
+    var data: `val`? = null: ArrayList<GamesItem>
+}
---
+++ TikTok/app/src/main/java/com/app/tiktok/model/GamesItem.kt
@@ -0,0 +1,21 @@
+package com.app.tiktok.model
+
+class GamesItem {
+    val createdAt: Int,
+    val description: String,
+    val externalIframeURL: String,
+    val gameId: Int,
+    val gamePlayCount: Int,
+    val gamePriority: Int,
+    val hidden: Boolean,
+    val iframeUrl: String,
+    val instructions: String,
+    val metaDescription: String,
+    val name: String,
+    val name_hindi: String,
+    val newGamesPriority: Int,
+    val pictureUrl: String,
+    val pictureUrl_sm: String,
+    val slug: String,
+    val updatedAt: Int
+}
